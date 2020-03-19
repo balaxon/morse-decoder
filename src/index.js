c42a50b868fd24b241605b6571adf46634ab8e84
balaxon <balahanov2209@gmail.com>
2020-03-19 15:03:53 +0300

    feat: all done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,16 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = '';
+  let space = /00/g;
+  let dot = /10/g;
+  let dush = /11/g;
+  let letter = expr.replace(dush, '-').replace(dot, '.').replace(space, '2');
+  let mass = letter.match(/.{5}/g).map(el => el.replace(/2/g, ''));
+  for (let i = 0; i < mass.length; i++) {
+    mass[i] == '*****' ? str += ' ' : str += MORSE_TABLE[mass[i]];
+  }
+  return str.replace(/\s{2}/g, ' ');
 }
 
 module.exports = {
